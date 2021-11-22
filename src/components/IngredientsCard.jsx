7d035f75b5eee8aaed03ab40c7b0d0d2dfb43cce
MuriloM <muriloommaia@gmail.com>
2021-11-22 20:01:42 -0300

    tela de ingredientes

--- src/components/IngredientsCard.jsx
+++ src/components/IngredientsCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card as div, CardGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -20,14 +20,14 @@ function IngredientsCards({ value, base, CARD_SHOW, path }) {
   }
 
   return (
-    <CardGroup>
+    <div className="bg-tertiary flex flex-wrap justify-center px-1 md:px-3 md:justify-around transform hover:scale-100 transition duration-500 ease-in-out pt-3">
       { value.map((recipe, index) => {
         if (index >= CARD_SHOW) return null;
         return (
           <Link to={ `/${path.red}` } key={ index }>
             <button
               type="button"
-              className="invisible-btn"
+              className="border border-quaternary opacity-90 blur-3xl mt-2 md:mb-3 ml-0 md:ml-4 shadow-xl rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:opacity-100 md:blur-none"
               data-testid={ `${index}-ingredient-card` }
               onClick={ () => {
                 dispatch(changeSearch({
@@ -38,25 +38,28 @@ function IngredientsCards({ value, base, CARD_SHOW, path }) {
                 }));
               } }
             >
-              <Card style={ { width: '18rem' } }>
-                <Card.Img
+              <div className="flex items-center w-80 sm:w-96 md:w-72">
+                <img
                   variant="top"
                   src={ `https://www.the${path.src}db.com/images/ingredients/${recipe[base.strTitle]}-Small.png` }
+                  className='rounded-md'
+
                   data-testid={ `${index}-card-img` }
                 />
-                <Card.Body>
-                  <Card.Title
+                <div className="flex w-full justify-center">
+                  <p
                     data-testid={ `${index}-card-name` }
+                    className="mx-auto font-sans md:font-serif text-2xl"
                   >
                     { recipe[base.strTitle] }
-                  </Card.Title>
-                </Card.Body>
-              </Card>
+                  </p>
+                </div>
+              </div>
             </button>
           </Link>
         );
       })}
-    </CardGroup>
+    </div>
   );
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
